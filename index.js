52f5edcf033050d55f64050309b2e7b4673beff3
beko123X <bekooobdu@gmail>
2026-03-15 03:15:57 +0200

    update port number

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
